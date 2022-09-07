6959f93d631b4fd61aac4bdef6a161f646daedc9
Bo Bayles <bbayles@gmail.com>
2022-09-07 09:13:11 -0500

    Add more Kinesis helpers

--- boto3_helpers/kinesis.py
+++ boto3_helpers/kinesis.py
@@ -1,3 +1,5 @@
+from itertools import chain, zip_longest
+
 from boto3 import client as boto3_client
 
 
@@ -39,3 +41,94 @@ def yield_all_shards(kinesis_client=None, **kwargs):
         if not next_token:
             break
         kwargs['NextToken'] = next_token
+
+
+def yield_available_shard_records(StreamName, ShardId, kinesis_client=None, **kwargs):
+    """Yield all available records from the given Kinesis stream shard.
+    Records will be pulled from until ``MillisBehindLatest`` is zero.
+
+    * *StreamName* is the name of the stream.
+    * *ShardId* is the ID of the shard.
+    * *kinesis_client* is a ``boto3.client('kinesis_client')`` instance. If not given,
+      one will be created with ``boto3.client('kinesis_client')``.
+    * *kwargs* are passed directly to the ``get_shard_iterator`` method. By default
+      you'll get records from the stream's ``TRIM_HORIZON``.
+
+    Reading from the earliest available record:
+
+    .. code-block:: python
+
+        from datetime import datetime, timedelta, timezone
+        from boto3_helpers.kinesis import yield_available_shard_records
+
+        for record in yield_available_shard_records('example-stream', 'shard-0001'):
+            print(record['SequenceNumber], record['Data], sep='\t')
+
+    """
+    kinesis_client = kinesis_client or boto3_client('kinesis')
+
+    kwargs.setdefault('ShardIteratorType', 'TRIM_HORIZON')
+    shard_iterator = kinesis_client.get_shard_iterator(
+        StreamName=StreamName, ShardId=ShardId, **kwargs
+    )['ShardIterator']
+
+    while True:
+        resp = kinesis_client.get_records(ShardIterator=shard_iterator)
+        yield from resp.get('Records', [])
+        if not resp['MillisBehindLatest']:
+            break
+        shard_iterator = resp['NextShardIterator']
+
+
+def yield_available_stream_records(StreamName, kinesis_client=None, **kwargs):
+    """Yield all available records from the given Kinesis stream.
+    Records will be pulled from each of the stream's shards until ``MillisBehindLatest``
+    is zero. The shards' records will be interleaved together (example: if a stream has
+    three shards, the first record yielded will be from shard A, the second will be from
+    shard B, the third will be from shard, the fourth will be from shard A, etc.).
+
+    * *StreamName* is the name of the stream.
+    * *kinesis_client* is a ``boto3.client('kinesis_client')`` instance. If not given,
+      one will be created with ``boto3.client('kinesis_client')``.
+    * *kwargs* are passed directly to the ``get_shard_iterator`` method. By default
+      you'll get records from the stream's ``TRIM_HORIZON``.
+
+    Reading from the earliest available record:
+
+    .. code-block:: python
+
+        from datetime import datetime, timedelta, timezone
+        from boto3_helpers.kinesis import yield_available_stream_records
+
+        for record in yield_available_stream_records('example-stream'):
+            print(record['SequenceNumber], record['Data], sep='\t')
+
+    Reading from a particular timestamp:
+
+    .. code-block:: python
+
+        from datetime import datetime, timedelta, timezone
+        from boto3_helpers.kinesis import yield_available_stream_records
+
+        for record in yield_available_stream_records(
+            'example-stream',
+            ShardIteratorType='AT_TIMESTAMP',
+            Timestamp=datetime.now(timezone.utc) - timedelta(hours=1),
+        ):
+            print(record['SequenceNumber], record['Data], sep='\t')
+
+    .. note::
+
+        This is a synchronous function, and may not be fast enough for real-time
+        processing of high volume streams.
+    """
+    all_shard_records = []
+    for shard in yield_all_shards(StreamName=StreamName, kinesis_client=kinesis_client):
+        shard_records = yield_available_shard_records(
+            StreamName, shard['ShardId'], kinesis_client=kinesis_client, **kwargs
+        )
+        all_shard_records.append(shard_records)
+
+    for item in chain.from_iterable(zip_longest(*all_shard_records)):
+        if item is not None:
+            yield item
--- tests/test_kinesis.py
+++ tests/test_kinesis.py
@@ -4,7 +4,11 @@ from unittest import TestCase
 from boto3 import client as boto3_client
 from botocore.stub import Stubber
 
-from boto3_helpers.kinesis import yield_all_shards
+from boto3_helpers.kinesis import (
+    yield_all_shards,
+    yield_available_shard_records,
+    yield_available_stream_records,
+)
 
 
 class KinesisTests(TestCase):
@@ -78,4 +82,189 @@ class KinesisTests(TestCase):
             )
 
         expected = page_1_resp['Shards'] + page_2_resp['Shards']
+        self.assertEqual(actual, expected)
+
+    def test_yield_available_shard_records(self):
+        # Set up the stubber
+        kinesis_client = boto3_client('kinesis', region_name='not-a-region')
+        stubber = Stubber(kinesis_client)
+        stream_name = 'example-stream'
+        shard_id = 'shard-0001'
+        dt = datetime(2022, 9, 7, 15, 11, 0, tzinfo=timezone.utc)
+        iterator_params_1 = {
+            'StreamName': stream_name,
+            'ShardId': shard_id,
+            'ShardIteratorType': 'AT_TIMESTAMP',
+            'Timestamp': dt,
+        }
+        iterator_resp_1 = {'ShardIterator': 'iterator-0001'}
+        stubber.add_response('get_shard_iterator', iterator_resp_1, iterator_params_1)
+
+        # The first call to get_records doesn't catch us up to the latest data,
+        # so another shard iterator is given.
+        records_params_1 = {'ShardIterator': 'iterator-0001'}
+        records_resp_1 = {
+            'Records': [
+                {
+                    'PartitionKey': shard_id,
+                    'SequenceNumber': '100000001',
+                    'Data': b'Record 1',
+                },
+                {
+                    'PartitionKey': shard_id,
+                    'SequenceNumber': '100000002',
+                    'Data': b'Record 2',
+                },
+            ],
+            'MillisBehindLatest': 1,
+            'NextShardIterator': 'iterator-0002',
+        }
+        stubber.add_response('get_records', records_resp_1, records_params_1)
+
+        # The second call does catch us up, so that's the end.
+        records_params_2 = {'ShardIterator': 'iterator-0002'}
+        records_resp_2 = {
+            'Records': [
+                {
+                    'PartitionKey': shard_id,
+                    'SequenceNumber': '100000003',
+                    'Data': b'Record 3',
+                },
+            ],
+            'MillisBehindLatest': 0,
+        }
+        stubber.add_response('get_records', records_resp_2, records_params_2)
+
+        # Do the deed
+        with stubber:
+            actual = list(
+                yield_available_shard_records(
+                    'example-stream',
+                    shard_id,
+                    ShardIteratorType='AT_TIMESTAMP',
+                    Timestamp=dt,
+                    kinesis_client=kinesis_client,
+                )
+            )
+
+        expected = [
+            records_resp_1['Records'][0],
+            records_resp_1['Records'][1],
+            records_resp_2['Records'][0],
+        ]
+        self.assertEqual(actual, expected)
+
+    def test_yield_available_stream_records(self):
+        # Set up the stubber
+        kinesis_client = boto3_client('kinesis', region_name='not-a-region')
+        stubber = Stubber(kinesis_client)
+        stream_name = 'example-stream'
+        stream_shards = ['shard-a', 'shard-b', 'shard-c']
+
+        # This stream has two shards
+        list_params = {'StreamName': stream_name}
+        list_resp = {
+            'Shards': [
+                {
+                    'ShardId': stream_shards[0],
+                    'HashKeyRange': {
+                        'StartingHashKey': '100000000',
+                        'EndingHashKey': '19999999',
+                    },
+                    'SequenceNumberRange': {
+                        'StartingSequenceNumber': '100000000',
+                        'EndingSequenceNumber': '19999999',
+                    },
+                },
+                {
+                    'ShardId': stream_shards[1],
+                    'HashKeyRange': {
+                        'StartingHashKey': '200000000',
+                        'EndingHashKey': '29999999',
+                    },
+                    'SequenceNumberRange': {
+                        'StartingSequenceNumber': '200000000',
+                        'EndingSequenceNumber': '29999999',
+                    },
+                },
+            ],
+        }
+        stubber.add_response('list_shards', list_resp, list_params)
+
+        # The function will request a shard iterator for the first shard, then
+        # get records from it.
+        iterator_params_1 = {
+            'StreamName': stream_name,
+            'ShardId': stream_shards[0],
+            'ShardIteratorType': 'TRIM_HORIZON',
+        }
+        iterator_resp_1 = {'ShardIterator': 'iterator-a-1'}
+        stubber.add_response('get_shard_iterator', iterator_resp_1, iterator_params_1)
+
+        records_params_1 = {'ShardIterator': 'iterator-a-1'}
+        records_resp_1 = {
+            'Records': [
+                {
+                    'PartitionKey': stream_shards[0],
+                    'SequenceNumber': '100000001',
+                    'Data': b'Record a1',
+                },
+                {
+                    'PartitionKey': stream_shards[0],
+                    'SequenceNumber': '100000002',
+                    'Data': b'Record a2',
+                },
+            ],
+            'MillisBehindLatest': 0,
+        }
+        stubber.add_response('get_records', records_resp_1, records_params_1)
+
+        # Shard iterator and records for the second shard
+        iterator_params_2 = {
+            'StreamName': stream_name,
+            'ShardId': stream_shards[1],
+            'ShardIteratorType': 'TRIM_HORIZON',
+        }
+        iterator_resp_2 = {'ShardIterator': 'iterator-b-1'}
+        stubber.add_response('get_shard_iterator', iterator_resp_2, iterator_params_2)
+
+        records_params_2 = {'ShardIterator': 'iterator-b-1'}
+        records_resp_2 = {
+            'Records': [
+                {
+                    'PartitionKey': stream_shards[1],
+                    'SequenceNumber': '200000001',
+                    'Data': b'Record b1',
+                },
+                {
+                    'PartitionKey': stream_shards[1],
+                    'SequenceNumber': '200000002',
+                    'Data': b'Record b2',
+                },
+                {
+                    'PartitionKey': stream_shards[1],
+                    'SequenceNumber': '200000003',
+                    'Data': b'Record b3',
+                },
+            ],
+            'MillisBehindLatest': 0,
+        }
+        stubber.add_response('get_records', records_resp_2, records_params_2)
+
+        # Do the deed. Records from the two shards are interleaved together:
+        # A, B, A, B...
+        with stubber:
+            actual = list(
+                yield_available_stream_records(
+                    'example-stream', kinesis_client=kinesis_client
+                )
+            )
+
+        expected = [
+            records_resp_1['Records'][0],
+            records_resp_2['Records'][0],
+            records_resp_1['Records'][1],
+            records_resp_2['Records'][1],
+            records_resp_2['Records'][2],
+        ]
         self.assertEqual(actual, expected)
