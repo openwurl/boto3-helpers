4257b9999894268ad2c535717384eece3a56a214
Bo Bayles <bbayles@gmail.com>
2025-05-05 16:41:53 -0500

    Add awslambda.publish_version_limited

--- boto3_helpers/awslambda.py
+++ boto3_helpers/awslambda.py
@@ -1,5 +1,7 @@
 from boto3 import client as boto3_client
 
+from boto3_helpers.pagination import yield_all_items
+
 
 def update_environment_variables(function_name, new_env, *, lambda_client=None):
     """Do a partial update of a Lambda function's environment variables. Return the
@@ -17,10 +19,7 @@ def update_environment_variables(function_name, new_env, *, lambda_client=None):
         from boto3_helpers.awslambda import update_environment_variables
 
         new_env = {'LOG_LEVEL': 'INFO', 'LOG_SERVER': '198.51.100.1'}
-        result_env = update_environment_variables(
-            'ExamplePlaybackConfig',
-            AdDecisionServerUrl='https://198.51.100.1:24601/ads/',
-        )
+        result_env = update_environment_variables('test-function', new_env)
         assert result_env['LOG_LEVEL'] == 'INFO'
         assert result_env['LOG_SERVER'] == '198.51.100.1'
         assert result_env['LOG_PORT'] == '24601'  # Or whatever it was before
@@ -45,3 +44,59 @@ def update_environment_variables(function_name, new_env, *, lambda_client=None):
         FunctionName=function_name, Environment={'Variables': env}
     )
     return env
+
+
+def publish_version_limited(function_name, keep_count, lambda_client=None, **kwargs):
+    """Publish a new version of a Lambda function. Afterward, delete older versions
+    such that only a limited number are left.
+
+    * *function_name* is the Lambda function name.
+    * *keep_count* is the number of versions to leave after creating the new version.
+    * *lambda_client* is a ``boto3.client('lambda')`` instance. If not given, one will
+      be created with ``boto3.client('lambda')``.
+    * *kwargs* are passed to the ``publish_version`` call.
+
+    Usage:
+
+    .. code-block:: python
+
+        from boto3_helpers.awslambda import publish_version_limited
+
+        version_resp = publish_version_limited('test-function', 4)
+
+    Deleting function versions is a destructive operation, so take care.
+
+    .. note::
+
+        It's possible for another API user to publish or remove function versions
+        in between this function's calls to the Lambda API. Be aware that
+        the operations aren't atomic.
+    """
+    lambda_client = lambda_client or boto3_client('lambda')
+
+    # Publish the new version
+    kwargs['FunctionName'] = function_name
+    publish_resp = lambda_client.publish_version(**kwargs)
+
+    # Get the current list of versions
+    all_versions = sorted(
+        yield_all_items(
+            lambda_client,
+            'list_versions_by_function',
+            'Versions',
+            FunctionName=function_name,
+        ),
+        key=lambda x: int(x['Version']),
+    )
+
+    # If there are not too many versions, bail out.
+    if len(all_versions) <= keep_count:
+        return publish_resp
+
+    # Remove all but the requested number of versions.
+    for version_info in all_versions[:-keep_count]:
+        lambda_client.delete_function(
+            FunctionName=function_name, Qualifier=version_info['Version']
+        )
+
+    return publish_resp
