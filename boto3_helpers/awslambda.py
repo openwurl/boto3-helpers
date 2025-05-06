cd6e7402b1ad18431c81e034e7abff9e22b0e993
Bo Bayles <bbayles@gmail.com>
2025-05-06 14:46:32 -0500

    Add awslambda.delete_old_versions (#26)

--- boto3_helpers/awslambda.py
+++ boto3_helpers/awslambda.py
@@ -1,5 +1,7 @@
 from boto3 import client as boto3_client
 
+from boto3_helpers.pagination import yield_all_items
+
 
 def update_environment_variables(function_name, new_env, *, lambda_client=None):
     """Do a partial update of a Lambda function's environment variables. Return the
@@ -17,10 +19,7 @@ def update_environment_variables(function_name, new_env, *, lambda_client=None):
         from boto3_helpers.awslambda import update_environment_variables
 
         new_env = {'LOG_LEVEL': 'INFO', 'LOG_SERVER': '198.51.100.1'}
-        result_env = update_environment_variables(
-            'ExamplePlaybackConfig',
-            AdDecisionServerUrl='https://198.51.100.1:24601/ads/',
-        )
+        result_env = update_environment_variables('test-function', new_env)
         assert result_env['LOG_LEVEL'] == 'INFO'
         assert result_env['LOG_SERVER'] == '198.51.100.1'
         assert result_env['LOG_PORT'] == '24601'  # Or whatever it was before
@@ -45,3 +44,56 @@ def update_environment_variables(function_name, new_env, *, lambda_client=None):
         FunctionName=function_name, Environment={'Variables': env}
     )
     return env
+
+
+def delete_old_versions(function_name, keep_count, lambda_client=None, **kwargs):
+    """Delete older published versions of a Lambda function.
+
+    * *function_name* is the Lambda function name.
+    * *keep_count* is the number of versions to keep active.
+    * *lambda_client* is a ``boto3.client('lambda')`` instance. If not given, one will
+      be created with ``boto3.client('lambda')``.
+
+    Returns a list of the versions that were removed.
+
+    Usage:
+
+    .. code-block:: python
+
+        from boto3_helpers.awslambda import delete_old_versions
+
+        version_resp = delete_old_versions('test-function', 4)
+
+    This is a destructive operation, so take care.
+
+    .. note::
+
+        It's possible for another API user to publish or remove function versions
+        in between this function's calls to the Lambda API. Be aware that
+        the operations aren't atomic.
+    """
+    lambda_client = lambda_client or boto3_client('lambda')
+
+    # Get the current list of versions
+    all_versions = sorted(
+        yield_all_items(
+            lambda_client,
+            'list_versions_by_function',
+            'Versions',
+            FunctionName=function_name,
+        ),
+        key=lambda x: int(x['Version']),
+    )
+
+    # If there are not too many versions, bail out.
+    ret = []
+    if len(all_versions) <= keep_count:
+        return ret
+
+    # Remove all but the requested number of versions.
+    for version_info in all_versions[:-keep_count]:
+        version_id = version_info['Version']
+        lambda_client.delete_function(FunctionName=function_name, Qualifier=version_id)
+        ret.append(version_id)
+
+    return ret
--- tests/test_awslambda.py
+++ tests/test_awslambda.py
@@ -4,7 +4,10 @@ from unittest import TestCase
 from boto3 import client as boto3_client
 from botocore.stub import Stubber
 
-from boto3_helpers.awslambda import update_environment_variables
+from boto3_helpers.awslambda import (
+    delete_old_versions,
+    update_environment_variables,
+)
 
 
 class AWSLambdaTests(TestCase):
@@ -50,3 +53,64 @@ class AWSLambdaTests(TestCase):
             )
 
         self.assertEqual(actual, combined_env)
+
+    def test_delete_old_versions(self):
+        # Set up the stubber
+        lambda_client = boto3_client('lambda', region_name='not-a-region')
+        stubber = Stubber(lambda_client)
+
+        function_name = 'TestFunction'
+
+        # First is the call for the first page of list_versions_by_function
+        list_params_1 = {'FunctionName': function_name}
+        list_resp_1 = {
+            'Versions': [{'Version': '1'}, {'Version': '2'}],
+            'NextMarker': 'page-2',
+        }
+        stubber.add_response('list_versions_by_function', list_resp_1, list_params_1)
+
+        # Next is the call for the second page of list_versions_by_function
+        list_params_2 = {'FunctionName': function_name, 'Marker': 'page-2'}
+        list_resp_2 = {
+            'Versions': [{'Version': '3'}, {'Version': '4'}, {'Version': '5'}]
+        }
+        stubber.add_response('list_versions_by_function', list_resp_2, list_params_2)
+
+        # Next are the calls for the delete_function command. Versions 4 and 5
+        # should be kept; the rest should be deleted.
+        delete_params_1 = {'FunctionName': function_name, 'Qualifier': '1'}
+        delete_resp_1 = {}
+        stubber.add_response('delete_function', delete_resp_1, delete_params_1)
+
+        delete_params_2 = {'FunctionName': function_name, 'Qualifier': '2'}
+        delete_resp_2 = {}
+        stubber.add_response('delete_function', delete_resp_2, delete_params_2)
+
+        delete_params_3 = {'FunctionName': function_name, 'Qualifier': '3'}
+        delete_resp_3 = {}
+        stubber.add_response('delete_function', delete_resp_3, delete_params_3)
+
+        # Do the deed - we expect to get the publish response back
+        with stubber:
+            actual = delete_old_versions(function_name, 2, lambda_client=lambda_client)
+
+        self.assertEqual(actual, ['1', '2', '3'])
+
+    def test_delete_old_versions_no_deletes(self):
+        # Test that no deletes are made if there are fewer than the limit
+        # Set up the stubber
+        lambda_client = boto3_client('lambda', region_name='not-a-region')
+        stubber = Stubber(lambda_client)
+
+        function_name = 'TestFunction'
+
+        # First is the call for the first page of list_versions_by_function
+        list_params_1 = {'FunctionName': function_name}
+        list_resp_1 = {'Versions': [{'Version': '4'}, {'Version': '5'}]}
+        stubber.add_response('list_versions_by_function', list_resp_1, list_params_1)
+
+        # Do the deed - we expect to get the publish response back
+        with stubber:
+            actual = delete_old_versions(function_name, 2, lambda_client=lambda_client)
+
+        self.assertEqual(actual, [])
